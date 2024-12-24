d44c474ed8be1383ca26ebb167237a2c1c5fa9b7
Increasingly unclear <kevinwalker1@mac.com>
2024-12-24 19:25:30 +0000

    Create test.ino

---
+++ xbee/test.ino
@@ -0,0 +1,15 @@
+// Simplest code for testing XBee via serial
+// Kevin Walker 03 Mar 2015
+
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+   Serial.println("Hooray");
+   // delay(2000);
+}
+
+  
